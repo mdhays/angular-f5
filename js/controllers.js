08607eed5e55f6fa179fabfa70b25d90b20bd403
Matt Hays <mdhays1995@gmail.com>
2016-12-01 14:22:22 -0600

    Added second partial and set up each li to be it's own link.

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,16 @@ var candidateControllers = angular.module('candidateControllers', []);
 candidateControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.candidates = data;
-    $scope.candidateOrder = "firstName";
+    $scope.candidateOrder = 'firstName';
     console.log(data);
+  });
+}]);
+
+candidateControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('js/data.json').success(function(data) {
+    $scope.candidates = data;
+    $scope.whichItem = $routeParams.itemId;
+    console.log(data);
+    console.log($scope.whichItem);
   });
 }]);
